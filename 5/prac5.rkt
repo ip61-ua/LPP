832117b46fc065e2e1681aa562e247f4653cf01a
ip61-ua <ip61@alu.ua.es>
2025-03-01 19:09:48 +0100

    Ejercicio 2 de práctica 5

--- 5/prac5.rkt
+++ 5/prac5.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require "lpp.rkt")
 
 ;; Ejercicio 1
 
@@ -66,10 +67,7 @@
 
 ;; Ejercicio 2
 
-
-;----;
-; b) ;
-;----;
+;; a)
 
 (define (inserta-ordenada n lista-ordenada)
   (cond
@@ -78,21 +76,74 @@
     (else (cons (first lista-ordenada)
                 (inserta-ordenada n (rest lista-ordenada))))))
 
-(inserta-ordenada 10 '(-8 2 3 11 20)) ;-> '(-8 2 3 10 11 20)
+; (inserta-ordenada 10 '()) ;-> '(10)
+; (inserta-ordenada 10 '(11)) ;-> '(10 11)
+; (inserta-ordenada 10 '(1)) ;-> '(1 10)
+; (inserta-ordenada 10 '(-8 2 3 11 20)) ;-> '(-8 2 3 10 11 20)
 
 (define (inserta-ordenada-genérica n lista-ordenada menor-igual?)
-  ...)
+  (cond
+    ((null? lista-ordenada) (cons n lista-ordenada))
+    ((menor-igual? n (first lista-ordenada)) (cons n lista-ordenada))
+    (else (cons (first lista-ordenada) (inserta-ordenada-genérica n (rest lista-ordenada) menor-igual?)))))
 
-;----;
-; c) ;
-;----;
+; (inserta-ordenada-genérica 10 '() <=) ;-> '(10)
+; (inserta-ordenada-genérica 10 '(11) <=) ;-> '(10 11)
+; (inserta-ordenada-genérica 10 '(1) <=) ;-> '(1 10)
+; (inserta-ordenada-genérica 10 '(-8 2 3 11 20) <=) ;-> '(-8 2 3 10 11 20)
 
+; (ordena '(2 -1 100 4 -6)) ;-> '(-6 -1 2 4 100)
 
-(define (ordena lista)
+(define (ordena-genérica lista menor-igual?)
   (if (null? lista)
       '()
-      (inserta-ordenada (first lista) (ordena (rest lista)))))
+      (inserta-ordenada-genérica (first lista) (ordena-genérica (rest lista) menor-igual?) menor-igual?)))
 
-(ordena '(2 -1 100 4 -6)) ;-> '(-6 -1 2 4 100)
+; (ordena-genérica '(2 -1 100 4 -6) <=) ;-> '(-6 -1 2 4 100)
+; (ordena-genérica '() <=) ;-> '()
+; (ordena-genérica '(1) <=) ;-> '(1)
+; (ordena-genérica '(2 1) <=) ;-> '(1 2)
 
-(define (ordena lista menor-igual?) ...)
+;; b)
+
+;(ordena-genérica '("Hola" "me" "llamo" "Iñigo" "Montoya") (lambda (a b) (<= (string-length a) (string-length b)) )) ; -> '("me" "Hola" "llamo" "Iñigo" "Montoya"))
+;(ordena-genérica '("Hola" "me" "llamo" "Iñigo" "Montoya") string<? ) ; -> '("Hola" "Iñigo" "Montoya" "llamo" "me"))
+;(ordena-genérica '((2 . 2) (1 . 1) (3 . 0) (5 . 1)) (lambda (a b) (< (+ (car a) (cdr a)) (+ (car b) (cdr b))) ) ) ; -> '((1 . 1) (3 . 0) (2 . 2) (5 . 1)))
+
+
+;; c)
+
+;; Cada vez que veo el ejercicio de cartas me entran ganas de cometer algún crimen.
+;; #NoMásExesDeCartas #NoEsFuncional #FelizLunes #MociónDeCensura
+
+;;;; IMPORTADO DE LA PRÁCTICA 2
+(define (obten-valor char)
+  (cond
+    ((equal? char #\A) 1)
+    ((equal? char #\J) 10)
+    ((equal? char #\Q) 11)
+    ((equal? char #\K) 12)
+    (else (- (char->integer char) (char->integer #\0)))))
+
+(define (obten-palo char)
+  (cond
+    ((equal? char #\♠) 'Picas)
+    ((equal? char #\♣) 'Tréboles)
+    ((equal? char #\♥) 'Corazones)
+    ((equal? char #\♦) 'Diamantes)))
+
+(define (cs->p s)
+  (cons (string-ref (symbol->string s) 0) (string-ref (symbol->string s) 1) ))
+
+(define (carta c)
+  (cons (obten-valor (car (cs->p c))) (obten-palo (cdr (cs->p c)))))
+
+(define (valor-carta c)
+  (car (carta c)))
+;;;; FIN IMPORTE
+
+(define (ordena-cartas l)
+  (ordena-genérica l (lambda (a b) (< (valor-carta a) (valor-carta b)))))
+
+(ordena-cartas '(Q♠ J♣ 5♣ Q♥ J♦)) ; ⇒ '(5♣ J♣ J♦ Q♠ Q♥)
+
