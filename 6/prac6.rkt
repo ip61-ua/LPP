0ccfbabe0e9ecdc4de862f7b1d45907f71615dcd
ip61-ua <ip61@alu.ua.es>
2025-03-13 11:43:42 +0100

    Ejercicio 2a parte 1

--- 6/prac6.rkt
+++ 6/prac6.rkt
@@ -10,8 +10,8 @@
       acc
       (concat-iter (rest l) (string-append acc (first l)))))
   
-(concat  '("hola" "y" "adiós")) ; ⇒ "holayadiós"
-(concat-iter '("hola" "y" "adiós") "") ; ⇒ "holayadiós"
+;(concat  '("hola" "y" "adiós")) ; ⇒ "holayadiós"
+;(concat-iter '("hola" "y" "adiós") "") ; ⇒ "holayadiós"
 
 ; 1b
 (define (min-max lista)
@@ -25,7 +25,20 @@
     (else (min-max-iter (rest l) acc)))
     )
 
-(min-max '(2 5 9 12 5 0 4)) ; ⇒ (0 . 12)
-(min-max '(3 2 -8 4 10 0))  ; ⇒ (-8 . 10)
-(min-max-iter '(5 9 12 -2 5 0 4) (cons 2 2)) ; ⇒ (-2 . 12)
+;(min-max '(2 5 9 12 5 0 4)) ; ⇒ (0 . 12)
+;(min-max '(3 2 -8 4 10 0))  ; ⇒ (-8 . 10)
+;(min-max-iter '(5 9 12 -2 5 0 4) (cons 2 2)) ; ⇒ (-2 . 12)
+
+; 2a
+
+(define (expande-pareja-iter p acc)
+  (if (or (null? p) (= (cdr p) 0))
+      acc
+      (expande-pareja-iter (cons (car p) (- (cdr p) 1)) (append acc (list (car p))))))
+
+(define (expande-pareja p)
+  (expande-pareja-iter p '()))
+
+(expande-pareja (cons 'a 4)) ; ⇒ (a a a a)
+;(expande-parejas '(#t . 3) '("LPP" . 2) '(b . 4)) ; ⇒ (#t #t #t "LPP" "LPP" b b b b)
 
