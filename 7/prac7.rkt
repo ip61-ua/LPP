439f39f49a082db12ca21899f69ea721dbc2f38f
ip61-ua <ip61@alu.ua.es>
2025-03-20 07:40:02 +0100

    Ejercicio 1 b y c

--- 7/prac7.rkt
+++ 7/prac7.rkt
@@ -6,5 +6,55 @@
 ; a
 (define lista-a '((a b) d (c (e) (f g) h)))
 (check-equal? (fourth (third lista-a)) 'h)
+;(pinta-lista lista-a)
 
-(pinta-lista lista-a)
+; b
+(define lista-b1 '((2 (3)) (4 2) ((2) 3)))
+; L1        *
+; L2  *     *     *
+; L3 2 *   4 2   * 3
+; L4   3         2
+(define lista-b2 '((b) (c (a)) d (a)))
+; L1         *
+; L2 *    *    d   *
+; L3 b   c *       a
+; L4       a
+(pinta-lista lista-b2)
+
+; c
+(define (cuadrado-estruct lista)
+  (cond ((null? lista) '())
+        ((hoja? lista) (* lista lista ))
+        (else (cons
+               (cuadrado-estruct (first lista))    ; El argumento aquí puede ser una hoja o lista 
+               (cuadrado-estruct (rest lista)))))) ; Aquí siempre es una lista
+
+(pinta-lista (cuadrado-estruct lista-b1))
+; 1 llamada
+; first: (cuadrado-estruct (first lista)) ; (2 (3))
+; rest:  (cuadrado-estruct (rest lista))  ; ((4 2) ((2) 3))
+; 2 llamada
+; first  2         ; devolver * 2 2
+; rest   ((3))
+; first  (4 2)
+; rest   (((2) 3))
+; 3 llamada
+; first (3)
+; rest  ()         ; devolver ()
+; first 4          ; devolver * 4 4
+; rest  (2)
+; first ((2) 3)
+; rest  ()         ; devolver ()
+; 4 llamada
+; first 3          ; devolver * 3 3
+; rest  ()         ; devolver ()
+; first 2          ; devolver * 2 2
+; rest  ()         ; devolver ()
+; first (2)
+; rest  (3)
+; 5 llamada
+; first 2          ; devolver * 2 2
+; rest  ()         ; devolver ()
+; first 3          ; devolver * 3 3
+; rest  ()         ; devolver ()
+
