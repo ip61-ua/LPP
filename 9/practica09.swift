a414dca96d64bb0b01292830c3d0bd504db705f6
ip61-ua <ip61@alu.ua.es>
2025-04-10 21:37:49 +0200

    Experimentos primeros.

--- 9/practica09.swift
+++ 9/practica09.swift
@@ -1,1 +1,22 @@
-print("Hola, mundo")
+/**
+ * Ejercicio 1
+ */
+
+// print("Hola, mundo")
+
+// Exp. 1 y 2
+let miFloat: Float = 4
+var miEtiqueta = "El valor es "
+
+var miEtiqueta2 = miEtiqueta
+
+// cannot convert value of type 'Float' to expected argument Al hacerlo sin String
+miEtiqueta = miEtiqueta + String(miFloat) + " " + miEtiqueta  // + = append
+//miEtiqueta.append(miFloat)
+
+//++miEtiqueta // da error
+//miEtiqueta = miEtiqueta - miEtiqueta // da error
+print("1 |", miEtiqueta)
+
+miEtiqueta2 = "\(miEtiqueta2)\(miFloat)"
+print("2 |", miEtiqueta2)
