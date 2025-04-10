b024bfb0daddcfcdf5f0806476e45aa57828246b
ip61-ua <ip61@alu.ua.es>
2025-04-10 22:49:27 +0200

    Añadir más experimentos.

--- 9/practica09.swift
+++ 9/practica09.swift
@@ -67,3 +67,48 @@ print("Hola desde casa")
 */
 
 // Exp. 6
+/*
+let numerosInteresantes = [
+  "Primos": [2, 3, 5, 7, 11, 13],
+  "Fibonacci": [1, 1, 2, 3, 5, 8],
+  "Cuadrados": [1, 4, 9, 16, 25],
+]
+var mayor = 0
+var mayorString = ""
+
+for (clase, numeros) in numerosInteresantes {
+  for num in numeros {
+    if num > mayor {
+      mayor = num
+      mayorString = clase
+    }
+  }
+}
+print("\"\(mayorString)\": \(mayor)")
+*/
+
+// Exp. 7
+/*
+var n = 2
+while n < 100 {
+  n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+  m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for _ in -1..<4 {
+  total += 1
+}
+print(total)
+
+func saluda(nombre: String, comida: String = "burger cangreburger") -> String {
+  return "Hola \(nombre), tienes para hoy \(comida)."
+}
+print(saluda(nombre: "Bob"))
+*/
