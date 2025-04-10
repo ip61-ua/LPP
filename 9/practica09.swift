8bb4f51d7e43ed8e271ef2bd3e3ee5b58d349ac1
ip61-ua <ip61@alu.ua.es>
2025-04-10 22:29:39 +0200

    Añadir más experimentos.

--- 9/practica09.swift
+++ 9/practica09.swift
@@ -24,8 +24,46 @@ print("2 |", miEtiqueta2)
 */
 
 // Exp. 3
+/*
 let nameSalute = "internauta"
 let miResult: Float = 9 / 2
 
 print("Resultado: \(miResult * 2) = 9")
 print("¡Saludos, \(nameSalute)!")
+*/
+
+// Exp. 4
+/*
+var cadenaOpcional: String? = "Hola"
+print(cadenaOpcional == nil)
+
+var nombreOpcional: String? = nil
+var saludo = "Hola!"
+if let nombre = nombreOpcional {
+  saludo = "Hola, \(nombre)"
+} else {
+  saludo += saludo  // si es nil, sale esto
+}
+print(saludo)
+*/
+
+// Exp. 5
+/*
+let nombrePila: String? = nil
+let nombreCompleto: String = "John Appleseed"
+let saludoInformal = "¿Qué tal, \(nombrePila == nil ? "undefined" : nombreCompleto)?"
+print(saludoInformal)
+
+let verdura = "zanahoria"
+switch verdura {  // Switch must be exhaustive
+case "zanahoria":
+  print("Buena para la vista.")
+case "lechuga", "tomates":
+  print("Podrías hacer una buena ensalada.")
+default:
+  print("Siempre puedes hacer una buena sopa.")
+}
+print("Hola desde casa")
+*/
+
+// Exp. 6
